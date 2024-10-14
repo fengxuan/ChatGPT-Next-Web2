07c37562466ff9781bdeff21c5e9613e35db4d75
FengXuan <xuanjianfeng@163.com>
2024-10-14 21:25:11 +0800

    update auth.tsx and access.ts

--- app/store/access.ts
+++ app/store/access.ts
@@ -103,7 +103,7 @@ const DEFAULT_ACCESS_STATE = {
 
   // server config
   needCode: true,
-  hideUserApiKey: true,
+  hideUserApiKey: false,
   hideBalanceQuery: false,
   disableGPT4: true,
   disableFastLink: false,
