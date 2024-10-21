6e18a8543dc5e780600dd41ff512563cd3f5d39a
FengXuan <xuanjianfeng@163.com>
2024-10-21 20:27:20 +0800

    friendly link

--- app/components/chat-list.tsx
+++ app/components/chat-list.tsx
@@ -169,7 +169,7 @@ export function ChatList(props: { narrow?: boolean }) {
         )}
       </Droppable>
 
-      <div className="border-t border-light-blue mt-4 pt-4 border-b border-light-blue">
+      <div className="border-t border-light-blue mt-4 pt-4">
         <h4 className="text-lg font-semibold mb-3 text-center">
           friendly link
         </h4>
@@ -179,7 +179,7 @@ export function ChatList(props: { narrow?: boolean }) {
                 href="https://babelchat.lat"
                 target="_blank"
                 rel="aichat.lat"
-                className="block  text-center py-2 px-4 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-100 transition-colors duration-200 no-underline"
+                className="block  text-center py-2 px-4 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-100 transition-colors duration-200"
               >
                 BabelChat
               </a>
@@ -189,7 +189,7 @@ export function ChatList(props: { narrow?: boolean }) {
                 href="https://q.spell.lat"
                 target="_blank"
                 rel="q.spell.lat"
-                className="block text-center py-2 px-4 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-100 transition-colors duration-200 no-underline"
+                className="block text-center py-2 px-4 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-100 transition-colors duration-200∂
               >
                 Q.Spell
               </a>
